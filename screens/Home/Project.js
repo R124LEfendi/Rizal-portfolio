ba295cf572ed80e285901e0b22ab6aacf54f2851
R124LEfendi <rizalgaras@gmail.com>
2023-05-14 10:47:02 +0700

    image url

--- screens/Home/Project.js
+++ screens/Home/Project.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import ContainerSection from "../../Components/ContainerSection";
+import Link from "next/link";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -14,6 +15,27 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Project() {
+  const datas = [
+    {
+      id: 1,
+      nama: "project1",
+      url: "www.facebook.com",
+      image: "QodiingiyahTeam.jpg",
+    },
+    {
+      id: 2,
+      nama: "project2",
+      url: "www.facebook.com",
+      image: "ogik.jpg",
+    },
+    {
+      id: 3,
+      nama: "project3",
+      url: "https://facebook.com",
+      image: "Rizal.jpg",
+    },
+  ];
+
   return (
     <ContainerSection title={"Project"}>
       <Box sx={{ flexGrow: 1 }}>
@@ -22,9 +44,13 @@ export default function Project() {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 14 }}
         >
-          {Array.from(Array(6)).map((_, index) => (
+          {datas.map((item, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item></Item>
+              <Item>
+                <a href={item.url}>
+                  <img src={item.image} />
+                </a>
+              </Item>
             </Grid>
           ))}
         </Grid>
